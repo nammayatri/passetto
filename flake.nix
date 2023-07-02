f62c9b5e4d6ffe315fd8496d35e3f51873317cf7
shivaraj-bh <sbh69840@gmail.com>
2023-07-03 00:31:45 +0530

    configure pg_hba

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     haskell-flake.url = "github:srid/haskell-flake";
 
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    services-flake.url = "github:juspay/services-flake";
+    services-flake.url = "github:juspay/services-flake/postgres/pgHba";
   };
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } ({ self, ... }: {
